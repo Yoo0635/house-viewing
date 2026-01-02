b89a39bd6718d5b986b29392f92aab02b9fb5fd0
Yoo <yooyoo9191@gmail.com>
2026-01-02 16:07:41 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/example/houseviewing/HouseViewingApplication.java
@@ -0,0 +1,13 @@
+package com.example.houseviewing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseViewingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseViewingApplication.class, args);
+	}
+
+}
